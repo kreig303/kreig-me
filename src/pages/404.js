3cd39e64e584ff985cd2b268251e22a1982c4e8d
Kreig Zimmerman <12191766+kreig303@users.noreply.github.com>
2020-03-29 23:57:12 -0400

    style(src/pages/404.js): modified 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,9 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="not found" />
+    <h1>404</h1>
+    <p>Not Found</p>
   </Layout>
 )
 
